27ff72f00cd3bbc61947aedaf4e4f574f78faf1c
anesben <benyethoanass@gmail.com>
2019-12-18 13:16:20 +0100

    swagger added

--- src/examples.php
+++ src/examples.php
@@ -1,111 +1,323 @@
-<?php
-use \Psr\Http\Message\ServerRequestInterface as Request;
-use \Psr\Http\Message\ResponseInterface as Response;
-
-use OpenApi\Annotations as OA;
-
-
-require '../vendor/autoload.php';
-
-$app = new \Slim\App;
-
+<?
 
 /**
- * @OA\Post(
- *     path="/get/event/{id}",
- *     @OA\Parameter(
- *      name="id",
- *      in="path",
- *      description="ID de l'evenement",
- *      required=true,
- *      @OA\Schema(type="integer")
- *      ),
- *     @OA\RequestBody(
- *         @OA\MediaType(
- *             mediaType="application/json",
- *             @OA\Schema(
- *                 @OA\Property(
- *                     property="id",
- *                     type="integer"
- *                 ),
- *                 @OA\Property(
- *                     property="name",
- *                     type="string"
- *                 ),
- *                 @OA\Property(
- *                     property="endroit",
- *                     type="string"
- *                 ),
- *                 example={"id": 10, "name": "Déjeuner", "endroit": "RU Mont Houy"}
- *             )
- *         )
- *     ),
- *     @OA\Response(
- *         response=200,
- *         description="Successfully added"
- *     )
- * )
-*/
-$app->get('/get/event/{id}', function (Request $request, Response $response, array $args) {
-    $id = $args['id'];
-    $response->getBody()->write("$id shown.");
-
-    return $response;
-});
-
+ * @license Apache 2.0
+ */
+namespace Petstore30\controllers;
 /**
- * @OA\Get(
- *     path="/get/allevents",
- *     @OA\RequestBody(
- *         @OA\MediaType(
- *             mediaType="application/json",
- *             @OA\Schema(
- *                 @OA\Property(
- *                     property="id",
- *                     type="integer"
- *                 ),
- *                 @OA\Property(
- *                     property="name",
- *                     type="string"
- *                 ),
- *                 @OA\Property(
- *                     property="endroit",
- *                     type="string"
- *                 ),
- *                 example={"id": 10, "name": "Déjeuner", "endroit": "RU Mont Houy"}
- *             )
- *         )
- *     ),
- *     @OA\Response(
- *         response=200,
- *         description="Successfully added"
- *     )
- * )
-*/
-
-$app->get('/get/allevents', function (Request $request, Response $response) {
-    $response->getBody()->write("All events");
-
-    return $response;
-});
-
-
-
-
-$app->get('/get/champion/{name}', function (Request $request, Response $response, array $args) {
-    $name = $args['name'];
-    $response->getBody()->write("Toi, $name tu es un champion !!");
-
-    return $response;
-});
-
-$app->get('/get/champion', function (Request $request, Response $response) {
-    $response->getBody()->write("Vous étes les champions !!!!");
-
-    return $response;
-});
-
-
-$app->run();
-
-
+ * Class Pet
+ *
+ * @package Petstore30\controllers
+ *
+ * @author  Donii Sergii <doniysa@gmail.com>
+ */
+class Pet
+{
+    /**
+     * @OA\Post(
+     *     path="/pet",
+     *     tags={"pet"},
+     *     summary="Add a new pet to the store",
+     *     operationId="addPet",
+     *     @OA\Response(
+     *         response=405,
+     *         description="Invalid input"
+     *     ),
+     *     security={
+     *         {"petstore_auth": {"write:pets", "read:pets"}}
+     *     },
+     *     requestBody={"$ref": "#/components/requestBodies/Pet"}
+     * )
+     */
+    public function addPet()
+    {
+    }
+    /**
+     * @OA\Put(
+     *     path="/pet",
+     *     tags={"pet"},
+     *     summary="Update an existing pet",
+     *     operationId="updatePet",
+     *     @OA\Response(
+     *         response=400,
+     *         description="Invalid ID supplied"
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Pet not found"
+     *     ),
+     *     @OA\Response(
+     *         response=405,
+     *         description="Validation exception"
+     *     ),
+     *     security={
+     *         {"petstore_auth": {"write:pets", "read:pets"}}
+     *     },
+     *     requestBody={"$ref": "#/components/requestBodies/Pet"}
+     * )
+     */
+    public function updatePet()
+    {
+    }
+    /**
+     * @OA\Get(
+     *     path="/pet/findByStatus",
+     *     tags={"pet"},
+     *     summary="Finds Pets by status",
+     *     description="Multiple status values can be provided with comma separated string",
+     *     operationId="findPetsByStatus",
+     *     deprecated=true,
+     *     @OA\Parameter(
+     *         name="status",
+     *         in="query",
+     *         description="Status values that needed to be considered for filter",
+     *         required=true,
+     *         explode=true,
+     *         @OA\Schema(
+     *             type="array",
+     *             default="available",
+     *             @OA\Items(
+     *                 type="string",
+     *                 enum = {"available", "pending", "sold"},
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="successful operation",
+     *         @OA\JsonContent(
+     *             type="array",
+     *             @OA\Items(ref="#/components/schemas/Pet")
+     *         ),
+     *         @OA\XmlContent(
+     *             type="array",
+     *             @OA\Items(ref="#/components/schemas/Pet")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Invalid status value"
+     *     ),
+     *     security={
+     *         {"petstore_auth": {"write:pets", "read:pets"}}
+     *     }
+     * )
+     */
+    public function findPetsByStatus()
+    {
+    }
+    /**
+     * @OA\Get(
+     *     path="/pet/findByTags",
+     *     tags={"pet"},
+     *     summary="Finds Pets by tags",
+     *     description=">-
+    Muliple tags can be provided with comma separated strings. Use\ \ tag1,
+    tag2, tag3 for testing.",
+     *     operationId="findByTags",
+     *     @OA\Parameter(
+     *         name="tags",
+     *         in="query",
+     *         description="Tags to filter by",
+     *         required=true,
+     *         explode=true,
+     *         @OA\Schema(
+     *             type="array",
+     *             @OA\Items(
+     *                 type="string",
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="successful operation",
+     *         @OA\JsonContent(
+     *             type="array",
+     *             @OA\Items(ref="#/components/schemas/Pet")
+     *         ),
+     *         @OA\XmlContent(
+     *             type="array",
+     *             @OA\Items(ref="#/components/schemas/Pet")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Invalid status value"
+     *     ),
+     *     security={
+     *         {"petstore_auth": {"write:pets", "read:pets"}}
+     *     }
+     * )
+     */
+    public function findByTags()
+    {
+    }
+    /**
+     * @OA\Get(
+     *     path="/pet/{petId}",
+     *     tags={"pet"},
+     *     summary="Find pet by ID",
+     *     description="Returns a single pet",
+     *     operationId="getPetById",
+     *     @OA\Parameter(
+     *         name="petId",
+     *         in="path",
+     *         description="ID of pet to return",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="integer",
+     *             format="int64"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="successful operation",
+     *         @OA\JsonContent(ref="#/components/schemas/Pet"),
+     *         @OA\XmlContent(ref="#/components/schemas/Pet"),
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Invalid ID supplier"
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Pet not found"
+     *     ),
+     *     security={
+     *         {"api_key": {}}
+     *     }
+     * )
+     *
+     * @param int $id
+     */
+    public function getPetById($id)
+    {
+    }
+    /**
+     * @OA\Post(
+     *     path="/pet/{petId}",
+     *     tags={"pet"},
+     *     summary="Updates a pet in the store with form data",
+     *     operationId="updatePetWithForm",
+     *     @OA\Parameter(
+     *         name="petId",
+     *         in="path",
+     *         description="ID of pet that needs to be updated",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="integer",
+     *             format="int64"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=405,
+     *         description="Invalid input"
+     *     ),
+     *     security={
+     *         {"petstore_auth": {"write:pets", "read:pets"}}
+     *     },
+     *     @OA\RequestBody(
+     *         description="Input data format",
+     *         @OA\MediaType(
+     *             mediaType="application/x-www-form-urlencoded",
+     *             @OA\Schema(
+     *                 type="object",
+     *                 @OA\Property(
+     *                     property="name",
+     *                     description="Updated name of the pet",
+     *                     type="string",
+     *                 ),
+     *                 @OA\Property(
+     *                     property="status",
+     *                     description="Updated status of the pet",
+     *                     type="string"
+     *                 )
+     *             )
+     *         )
+     *     )
+     * )
+     */
+    public function updatePetWithForm()
+    {
+    }
+    /**
+     * @OA\Delete(
+     *     path="/pet/{petId}",
+     *     tags={"pet"},
+     *     summary="Deletes a pet",
+     *     operationId="deletePet",
+     *     @OA\Parameter(
+     *         name="api_key",
+     *         in="header",
+     *         required=false,
+     *         @OA\Schema(
+     *             type="string"
+     *         )
+     *     ),
+     *     @OA\Parameter(
+     *         name="petId",
+     *         in="path",
+     *         description="Pet id to delete",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="integer",
+     *             format="int64"
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Invalid ID supplied",
+     *     ),
+     *     @OA\Response(
+     *         response=404,
+     *         description="Pet not found",
+     *     ),
+     *     security={
+     *         {"petstore_auth": {"write:pets", "read:pets"}}
+     *     },
+     * )
+     */
+    public function deletePet()
+    {
+    }
+    /**
+     * @OA\Post(
+     *     path="/pet/{petId}/uploadImage",
+     *     tags={"pet"},
+     *     summary="uploads an image",
+     *     operationId="uploadFile",
+     *     @OA\Parameter(
+     *         name="petId",
+     *         in="path",
+     *         description="ID of pet to update",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="integer",
+     *             format="int64",
+     *             example=1
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="successful operation",
+     *         @OA\JsonContent(ref="#/components/schemas/ApiResponse")
+     *     ),
+     *     security={
+     *         {"petstore_auth": {"write:pets", "read:pets"}}
+     *     },
+     *     @OA\RequestBody(
+     *         description="Upload images request body",
+     *         @OA\MediaType(
+     *             mediaType="application/octet-stream",
+     *             @OA\Schema(
+     *                 type="string",
+     *                 format="binary"
+     *             )
+     *         )
+     *     )
+     * )
+     */
+    public function uploadFile()
+    {
+    }
+}
